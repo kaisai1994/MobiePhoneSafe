07e8280c846b61344e677ca3a2d4ffdbb0a35a00
kaisai1994 <1137525948@qq.com>
2016-05-24 12:59:20 +0800

    Initial commit

---
+++ app/src/main/java/phonesafe/kagura/com/mobiephonesafe/MainActivity.java
@@ -0,0 +1,13 @@
+package phonesafe.kagura.com.mobiephonesafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
